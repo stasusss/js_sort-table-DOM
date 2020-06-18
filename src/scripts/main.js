e55a53ee11d070e834f3e9f16957760a7ce003f5
Stanislav <stasusss07@gmail.com>
2020-06-18 20:59:29 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello mate academy!');
+const table = document.getElementById('cars');
+const body = document.querySelector('tbody');
+const rows = [...body.children];
+
+table.addEventListener('click', (e) => {
+  const items = e.target;
+
+  if (items.tagName !== 'TH') {
+    return;
+  }
+
+  rows.sort((a, b) =>
+    (a.children[items.cellIndex]).textContent
+      .localeCompare(b.children[items.cellIndex].textContent));
+
+  for (const row of rows) {
+    body.append(row);
+  }
+});
